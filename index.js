eaa4052024f38513677e6a5d8f7c75988a7a340b
Simone Veitch <simone.veitch@gmail.com>
2024-03-21 11:51:14 +1100

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+class Polygon {
+    constructor(sides) {
+      this.sides = sides;
+    }
+  
+    get countSides() {
+      return this.sides.length;
+    }
+  
+    get perimeter() {
+      return this.sides.reduce((acc, curr) => acc + curr, 0);
+    }
+  }
+  
+  class Triangle extends Polygon {
+    get isValid() {
+      if (this.countSides !== 3) return false;
+      const [a, b, c] = this.sides;
+      return a + b > c && a + c > b && b + c > a;
+    }
+  }
+  
+  class Square extends Polygon {
+    get isValid() {
+      if (this.countSides !== 4) return false;
+      const side = this.sides[0];
+      return this.sides.every(s => s === side);
+    }
+  
+    get area() {
+      if (!this.isValid) return NaN;
+      return this.sides[0] ** 2;
+    }
+  }
+  
